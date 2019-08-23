15f26fde08a8679b6bb6a58e4329589b11d11009
QMaximillian <quinnlashinsky@gmail.com>
2019-08-22 20:19:44 -0400

    Configure App.js for websocket

--- src/index.js
+++ src/index.js
@@ -6,18 +6,48 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
 import {ApolloProvider} from 'react-apollo'
+import { getMainDefinition } from "apollo-utilities";
+import { ApolloLink, split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
 
-const MONITOR_BASE_URL = "http://localhost:8000/graphql";
+
+
+
+const MONITOR_BASE_URL = `http://localhost:8000/graphql`
+
+const httpLink = new HttpLink({
+  uri: MONITOR_BASE_URL,
+  headers: {
+    authorization: localStorage.getItem("token")
+      ? localStorage.getItem("token")
+      : ""
+  }
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8000/graphql`,
+  options: {
+    reconnect: true
+  }
+})
+
+const terminatingLink = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return (
+      kind === 'OperationDefinition' && operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const link = ApolloLink.from([terminatingLink])
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
-  link: new HttpLink({
-    uri: MONITOR_BASE_URL,
-    headers: {
-      authorization: localStorage.getItem("token") ? localStorage.getItem("token") : ""
-    }
-  }),
+  link
 });
 
 
